2908a02e136c017372bcd1d74ca7a36446be3d1b
SohamDas00 <jisusoham04@gmail.com>
2025-06-30 13:06:10 +0530

    update js file for project1

--- script1.js
+++ script1.js
@@ -6,21 +6,20 @@ box.addEventListener("mousemove",(details)=>{
         let red_color=gsap.utils.mapRange(corr.width/2,corr.width,0,255,corr_x);
         gsap.to(box,{
             backgroundColor:`rgb(${red_color},0,0)`,
-            ease:"power2",
+            ease:"power4",
         }) 
     }
     else{
         let blue_color=gsap.utils.mapRange(0,corr.width/2,255,0,corr_x);
         gsap.to(box,{
             backgroundColor:`rgb(0,0,rgb${blue_color})`,
-            ease:"power2",
+            ease:"power4",
         }) 
     }
 })
 box.addEventListener("mouseleave",()=>{
     gsap.to(box,{
         backgroundColor:`rgb(255,255,255)`,
-        ease:"power2",
+        ease:"power4",
     })
 })
-let value=document.querySelector(".heading")
